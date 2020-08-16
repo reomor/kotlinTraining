5abc35ab9c363a501d2d8cb02ff6bea01bc5c995
Reomor <reomor@gmail.com>
2020-08-16 16:02:00 +0300

    taxipark improvements

--- kotlinForJavaDevelopers/src/main/kotlin/kotlinForJava/hw/taxipark/TaxiParkTask.kt
+++ kotlinForJavaDevelopers/src/main/kotlin/kotlinForJava/hw/taxipark/TaxiParkTask.kt
@@ -17,38 +17,37 @@ fun TaxiPark.findFakeDrivers(): Set<Driver> {
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-        if (minTrips == 0) {
-            allPassengers.toSet()
-        } else {
-            trips.flatMap { it.passengers }
-                    .groupBy { it }
-                    .filter { it.value.size >= minTrips }
-                    .map { it.key }
-                    .toSet()
-        }
+    if (minTrips == 0) {
+        allPassengers.toSet()
+    } else {
+        trips.flatMap(Trip::passengers)
+            .groupBy { passenger -> passenger }
+            .filterValues { group -> group.size >= minTrips }
+            .keys
+    }
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-        trips.filter { it.driver == driver }
-                .flatMap { it.passengers }
-                .groupBy { it }
-                .filter { it.value.size > 1 }
-                .map { it.key }
-                .toSet()
+    trips.filter { it.driver == driver }
+        .flatMap(Trip::passengers)
+        .groupBy { passenger -> passenger }
+        .filterValues { group -> group.size > 1 }
+        .keys
+/*
+allPassengers.filter { passenger -> trips.count { it.driver == driver && passenger in it.passengers } > 1 }.toSet()
+ */
 
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
 fun TaxiPark.findSmartPassengers(): Set<Passenger> =
-        trips.flatMap { trip -> trip.passengers.associateWith { trip }.toList() }
-                .asSequence()
-                .groupBy({ it.first }, { it.second })
-                .map { entry -> entry.key to entry.value.partition { it.discount == null } }
-                .filter { it.second.first.size < it.second.second.size }
-                .map { it.first }
-                .toSet()
+    allPassengers.associateWith { passenger -> trips.filter { trip -> passenger in trip.passengers } }
+        .filterValues { group ->
+            val (withDiscount, withoutDiscount) = group.partition { it.discount != null }
+            withDiscount.size > withoutDiscount.size
+        }.keys
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
@@ -58,10 +57,13 @@ fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
     return if (trips.isEmpty()) {
         null
     } else {
-        trips.map { IntRange(it.duration / 10 * 10, it.duration / 10 * 10 + 9) to it }
-                .groupBy { it.first }
-                .maxBy { it.value.size }
-                ?.component1()
+        trips
+            .groupBy {
+                val rangeStart = it.duration / 10 * 10
+                IntRange(rangeStart, rangeStart + 9)
+            }
+            .maxBy { (_, group) -> group.size }
+            ?.key
     }
 }
 
@@ -70,17 +72,15 @@ fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
  * Check whether 20% of the drivers contribute 80% of the income.
  */
 fun TaxiPark.checkParetoPrinciple(): Boolean {
-    val totalCost = trips.map { it.cost }.sum()
-    return if (trips.isEmpty()) {
-        false
-    } else {
-        val driverCost = trips.map { it.driver to it.cost }
-                .groupBy({ it.first }, { it.second })
-                .map { it.key to it.value.sum() }
-                .toList()
-                .sortedByDescending { it.second }
-                .take((allDrivers.size * 0.2).roundToInt())
-                .sumByDouble { it.second }
-        driverCost >= totalCost * 0.8
-    }
+    if (trips.isEmpty()) return false
+    val totalCost = trips.sumByDouble(Trip::cost )
+    val driverCost = trips.asSequence()
+        .map {trip -> trip.driver to trip.cost }
+        .groupBy({ it.first }, { it.second })
+        .map { it.key to it.value.sum() }
+        .toList()
+        .sortedByDescending { it.second }
+        .take((allDrivers.size * 0.2).roundToInt())
+        .sumByDouble { it.second }
+    return driverCost >= totalCost * 0.8
 }
